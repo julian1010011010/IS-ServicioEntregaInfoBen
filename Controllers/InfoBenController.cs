41481f4dbcb2a333c77fbe727e3aa2fee36a9d07
Julian <Julian>
2022-10-27 18:29:34 -0500

    Fix  Bug Get Beneficiarios

--- Controllers/InfoBenController.cs
+++ Controllers/InfoBenController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using MySqlConnector;
 using System;
@@ -12,25 +13,30 @@ namespace WebApplication.Controllers
     [Route("[controller]")]
     public class InfoBenControllerController : ControllerBase
     {
+
+        private readonly string ConnectionStrings = "Server=bdasociatividad.c6m5z9oxzvab.us-west-2.rds.amazonaws.com;User ID=admin;Password=abcdef12345;Database=bdasociatividad";
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-      /// <summary>
-      /// Get all user 
-      /// </summary>
-      /// <returns></returns>
+        /// <summary>
+        /// Get all user 
+        /// </summary>
+        /// <returns></returns>
+        /// 
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)] 
         [HttpGet]
         public async Task<IActionResult> Get()
         {
             try
             {
-                using var connection = new MySqlConnection("Server=bdasociatividad.c6m5z9oxzvab.us-west-2.rds.amazonaws.com;User ID=admin;Password=abcdef12345;Database=bdasociatividad");
+                using var connection = new MySqlConnection(ConnectionStrings);
                 Console.Write(connection);
                 await connection.OpenAsync();
 
-                using var command = new MySqlCommand("SELECT * FROM t_Ben_Asoc;", connection);
+                using var command = new MySqlCommand("SELECT * FROM BeneficiariosAsociatividad;", connection);
                 using var reader = await command.ExecuteReaderAsync();
                 List<dynamic> Resposne = new();
 
@@ -45,7 +51,7 @@ namespace WebApplication.Controllers
                         NombreCompleto = reader.GetValue(2) + " " + reader.GetValue(3) + " " + reader.GetValue(4) + " " + reader.GetValue(5),
                         CorreoElectronico = reader.GetValue(8),
                         Municipio = reader.GetValue(9),
-                        Departamento = reader.GetValue(10), 
+                        Departamento = reader.GetValue(10),
                     };
                     Resposne.Add(Tabla);
                 }
@@ -57,5 +63,48 @@ namespace WebApplication.Controllers
                 return BadRequest(e.InnerException.ToString());
             }
         }
+
+        /// <summary>
+        /// Get all Municipios
+        /// </summary>
+        /// <returns></returns>
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [HttpGet("GetMunicipios")]
+        public async Task<IActionResult> GetMunicipios()
+        {
+            try
+            {
+                using var connection = new MySqlConnection(ConnectionStrings);
+                Console.Write(connection);
+                await connection.OpenAsync();
+
+                using var command = new MySqlCommand("SELECT * FROM MunicipiosDeptos;", connection);
+                using var reader = await command.ExecuteReaderAsync();
+                List<dynamic> Resposne = new();
+
+                while (await reader.ReadAsync())
+                {
+                    var Cedular = reader.GetValue(1);
+
+                    var Tabla = new
+                    {
+                        CodigoMunicipio = reader.GetValue(0),
+                        Municipio = reader.GetValue(1),
+                        Departamento = reader.GetValue(3)
+                    };
+                    Resposne.Add(Tabla);
+                }
+                return Ok(Resposne);
+
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.InnerException.ToString());
+            }
+        }
+
+
+
     }
 }
