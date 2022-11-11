f160e6c75c3584a4dce30366bf121f5315eddb86
laura <lalcoto@gmail.com>
2022-11-11 18:54:14 -0500

    Fix environment file

--- Web/src/environments/environment.prod.ts
+++ Web/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://preasivamosback.ffie.com.co',
+  apiUrl: 'https://preasivamosback.ffie.com.co',
 };
--- Web/src/environments/environment.ts
+++ Web/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://preasivamosback.ffie.com.co',
+  apiUrl: 'https://preasivamosback.ffie.com.co',
 };
 
 /*
